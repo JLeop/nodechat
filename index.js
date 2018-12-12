80d36fa1313c15e2f1a04e3e0c6501d552dd4924
Julian <julian@rega-sense.ch>
2018-12-12 22:06:31 +0100

    deployment

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+const port = process.env.PORT || 4000
+// App setup
+var app = express();
+var server = app.listen(port,function(){
+    console.log('listening for requests on port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.set('origins', '*:*');
+io.on('connection', function(socket){
+
+    console.log('made socket connection', socket.id);
+
+//Handle chat evet
+    socket.on('chat',function(data){
+      io.sockets.emit('chat', data);
+    });
+
+//Handle typing event
+    socket.on('typing', function(data){
+      socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,50 @@
+// Make connection
+var socket = io.connect('https://mr-robot-shsg.herokuapp.com/');
+
+//Query DOM
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+var clicks = 0;
+
+function updateScroll(){
+  element = document.getElementById('chat-window');
+  element.scrollTop = element.scrollHeight;
+}
+
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+  message.value = "";
+});
+
+message.addEventListener('keypress', function(e){
+  if (e.keyCode === 13&& !e.shiftKey) {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+  message.value = "";
+  }
+});
+
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+  feedback.innerHTML = "";
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' +  data.message + '</p>';
+  updateScroll();
+});
+
+socket.on('typing',function(data){
+  feedback.innerHTML ='<p><em>'+data+' is typing a message...</em></p>';
+});
